572ae54b0ad38706e105a69e44f359031c0f1545
Lewis J. Hannah <lewis.j.hannah@gmail.com>
2023-02-05 18:26:16 -0500

    Login with Supabase WIP

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AuthService } from '../services/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -9,7 +10,7 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 export class LoginComponent {
 
   form: FormGroup;
-  constructor(private fb: FormBuilder) {
+  constructor(private fb: FormBuilder, private auth: AuthService) {
     this.form = this.fb.group({
       email: ['lewis.j.hannah@gmail.com', [Validators.required, Validators.email]],
       password: ['precious5', [Validators.required, Validators.minLength(6)]]
@@ -19,7 +20,15 @@ export class LoginComponent {
   ngInit(): void {
   }
   
-  login() {
+  async login() {
     console.log(this.form.value);
+    const ret = await this.auth.login(this.form.value);
+    console.log(ret);
+  }
+
+  async register() {
+    console.log(this.form.value);
+    const ret = await this.auth.createAccount(this.form.value);
+    console.log(ret);
   }
 }
---
+++ src/environments/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+    production: false,
+    supabaseUrl: 'https://tigibdgtuezpmzvbrhyf.supabase.co',
+    supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRpZ2liZGd0dWV6cG16dmJyaHlmIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzU2MzQ1NjksImV4cCI6MTk5MTIxMDU2OX0.4wJXQGh0BWYVY-3UJNy6ZwPOn4403ZvrEx2vY4IYv24'
+}
