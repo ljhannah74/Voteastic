bca7792e7c66abec919f75bfcfec37eb00cb0871
Lewis J. Hannah <lewis.j.hannah@gmail.com>
2023-02-06 21:38:16 -0500

    Login with Supabase WIP

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 import { AuthService } from '../services/auth.service';
 
 @Component({
@@ -8,9 +9,8 @@ import { AuthService } from '../services/auth.service';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent {
-
   form: FormGroup;
-  constructor(private fb: FormBuilder, private auth: AuthService) {
+  constructor(private fb: FormBuilder, private auth: AuthService, private router: Router) {
     this.form = this.fb.group({
       email: ['lewis.j.hannah@gmail.com', [Validators.required, Validators.email]],
       password: ['precious5', [Validators.required, Validators.minLength(6)]]
@@ -22,13 +22,25 @@ export class LoginComponent {
   
   async login() {
     console.log(this.form.value);
-    const ret = await this.auth.login(this.form.value);
-    console.log(ret);
+    const {data, error} = await this.auth.login(this.form.value);
+    console.log(error);
+    if(error) {
+      // TODO show error alert
+    } else {
+      this.router.navigateByUrl('/app', {replaceUrl: true});
+    }
+
   }
 
   async register() {
     console.log(this.form.value);
-    const ret = await this.auth.createAccount(this.form.value);
-    console.log(ret);
+    const {data, error} = await this.auth.createAccount(this.form.value);
+    console.log(error);
+    if(error) {
+      // TODO show error alert
+    } else {
+      console.log(data.session);
+      this.router.navigateByUrl('/app', {replaceUrl: true});
+    }
   }
 }
